63eea4a391051bb54ed6ef952c2449a1054ea2e4
임승호 <132066506+YIM2UL2ET@users.noreply.github.com>
2023-10-24 00:00:10 +0900

    [Bronze V] Title: 검증수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a, b, c, d, e, sum;
+    scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
+    sum = a * a + b * b + c * c + d * d + e * e;
+    printf("%d", sum % 10);
+    return 0;
+}
