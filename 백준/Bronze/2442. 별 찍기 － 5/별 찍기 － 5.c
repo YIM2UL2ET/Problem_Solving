01fabf98bd9e49b892039b6761344deb11f098da
임승호 <132066506+YIM2UL2ET@users.noreply.github.com>
2023-10-24 17:09:23 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n, i, j;
+    scanf("%d", &n);
+    for (i = 0; i < n; i++)
+    {
+        for (j = 1; j < n; j++)
+        {
+            if (j < n - i) printf(" ");
+            else printf("**");
+        }
+        printf("*\n");
+    }
+    return 0;
+}
