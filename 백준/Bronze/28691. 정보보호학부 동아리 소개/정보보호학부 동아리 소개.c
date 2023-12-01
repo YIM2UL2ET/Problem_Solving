bc35679842f92acddf9b7c82f32d6be07db84d24
임승호 <132066506+YIM2UL2ET@users.noreply.github.com>
2023-12-01 21:25:41 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char n;
+    scanf("%c", &n);
+    if (n == 'M') printf("MatKor");
+    else if (n == 'W') printf("WiCys");
+    else if (n == 'C') printf("CyKor");
+    else if (n == 'A') printf("AlKor");
+    else printf("$clear");
+    return 0;
+}
