4edf01825b8b3ab452f9f1cd0081fd6305ec5e66
임승호 <132066506+YIM2UL2ET@users.noreply.github.com>
2024-02-24 15:38:27 +0900

    [Silver III] Title: 회전하는 큐, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.cc
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.cc
@@ -4,24 +4,21 @@
 
 int main(void)
 {
-    int n, m, k, idx, front, result;
+    int n, m, k, front, result;
     std::vector <int> vec;
 
     std::cin >> n >> m;
-    for (int i = 0; i < n;) {
-        vec.push_back(++i);
-    }
+    for (int i = 0; i < n;) vec.push_back(++i);
 
     result = 0, front = 0;
     for (int i = 0; i < m; i++) {
         std::cin >> k;
 
-        std::vector <int> :: iterator iter;
-        iter = std::find(vec.begin(), vec.end(), k);
-        idx = std::distance(vec.begin(), iter);
+        int idx = std::distance(vec.begin(), std::find(vec.begin(), vec.end(), k));
+        int distance = abs(front - idx);
 
-        if (abs(front - idx) < vec.size() - abs(front - idx)) result += abs(front - idx);
-        else result += vec.size() - abs(front - idx);
+        if (distance < vec.size() - distance) result += distance;
+        else result += vec.size() - distance;
 
         front = idx;
         vec.erase(vec.begin()+front);
