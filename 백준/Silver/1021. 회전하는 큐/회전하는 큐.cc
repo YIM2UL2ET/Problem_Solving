c0c7cbf6a835a5ce12822153819a4a472c13b4c1
임승호 <132066506+YIM2UL2ET@users.noreply.github.com>
2024-02-24 15:34:10 +0900

    [Silver III] Title: 회전하는 큐, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.cc
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.cc
@@ -1,43 +1,30 @@
 #include <iostream>
 #include <algorithm>
-#include <deque>
+#include <vector>
 
 int main(void)
 {
-    int n, m, k, idx, result;
-    std::deque <int> dq;
+    int n, m, k, idx, front, result;
+    std::vector <int> vec;
 
     std::cin >> n >> m;
     for (int i = 0; i < n;) {
-        dq.push_back(++i);
+        vec.push_back(++i);
     }
 
-    result = 0;
+    result = 0, front = 0;
     for (int i = 0; i < m; i++) {
         std::cin >> k;
 
-        std::deque <int> :: iterator iter;
-        iter = std::find(dq.begin(), dq.end(), k);
-        idx = std::distance(dq.begin(), iter);
+        std::vector <int> :: iterator iter;
+        iter = std::find(vec.begin(), vec.end(), k);
+        idx = std::distance(vec.begin(), iter);
 
-        if (idx < dq.size() - idx) {
-            result += idx;
-            for (int j = 0; j < idx; j++) {
-                int temp = dq.front();
-                dq.pop_front();
-                dq.push_back(temp);
-            }
-        }
-        else {
-            result += dq.size() - idx;
-            for (int j = 0; j < dq.size() - idx; j++) {
-                int temp = dq.back();
-                dq.pop_back();
-                dq.push_front(temp);
-            }
-        }
+        if (abs(front - idx) < vec.size() - abs(front - idx)) result += abs(front - idx);
+        else result += vec.size() - abs(front - idx);
 
-        dq.pop_front();
+        front = idx;
+        vec.erase(vec.begin()+front);
     }
     
     std::cout << result;
