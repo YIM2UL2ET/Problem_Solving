706b4a5258af6ec1eddb4677884abfaba3b8f217
임승호 <132066506+YIM2UL2ET@users.noreply.github.com>
2024-04-24 19:35:44 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int n;
+    cin >> n;
+    if (!n) cout << "YONSEI";
+    else cout << "Leading the Way to the Future";
+    return 0;
+}
