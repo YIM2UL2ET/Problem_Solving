3389dcafe85c1ed1b6fe2d10f8a365380a5fe2f4
임승호 <132066506+YIM2UL2ET@users.noreply.github.com>
2024-06-16 20:06:29 +0900

    [Gold II] Title: 친구 네트워크, Time: 240 ms, Memory: 38828 KB -BaekjoonHub

---
+++ 백준/Gold/4195. 친구 네트워크/친구 네트워크.cc
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+unordered_map<string, string> root;
+unordered_map<string, int> treeSize;
+
+string findRoot(string &n) {
+  if (n != root[n]) {
+    root[n] = findRoot(root[n]);
+  }
+  return root[n];
+}
+
+void solve() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int N;
+  cin >> N;
+
+  string A, B;
+  for (int i = 0; i < N; i++) {
+    cin >> A >> B;
+
+    if (root.find(A) == root.end()) {
+      root.insert({A, A});
+      treeSize.insert({A, 1});
+    }
+    if (root.find(B) == root.end()) {
+      root.insert({B, B});
+      treeSize.insert({B, 1});
+    }
+
+    string rootA, rootB;
+    rootA = findRoot(A);
+    rootB = findRoot(B);
+
+    if (rootA != rootB) {
+      root[rootB] = rootA;
+      treeSize[rootA] += treeSize[rootB];
+    }
+
+    cout << treeSize[rootA] << '\n';
+  }
+
+  root.clear();
+  treeSize.clear();
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int T;
+  cin >> T;
+
+  for (int i = 0; i < T; i++) {
+    solve();
+  }
+  return 0;
+}
