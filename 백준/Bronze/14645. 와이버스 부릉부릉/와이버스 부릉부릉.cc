6013cb56679b50f1e65e9b12879cc83a13e0431b
임승호 <132066506+YIM2UL2ET@users.noreply.github.com>
2024-06-28 03:16:14 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  int n, k;
+  cin >> n >> k;
+
+  for (int i = 0; i < n; i++) {
+    int a, b;
+    cin >> a >> b;
+  }
+
+  cout << "비와이";
+  return 0;
+}
